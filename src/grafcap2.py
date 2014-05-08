ef0ccc6c73a7db91388a07502d13ee84b69e2fa3
Carlos Herrera <alu0100828955@galois01.aulas.priv>
2014-05-08 14:51:29 +0100

    Capitulo 2 terminado con grafica

--- src/grafcap2.py
+++ src/grafcap2.py
@@ -1,11 +1,20 @@
 #! encoding: UTF-8
-import matplotlib.pylab as pl
-x=[-2.-1,0,1,2]
+import matplotlib.pyplot as pl
+x=[-2.0,-1.0,0.0,1.0,2.0]
 y=[-4.96,-4.8,-4,0,20]
-pl.title('Funcion (5^x)-5')
+x1=[-3,3]
+y1=[0,0]
+x2=[0,0]
+y2=[-6,30]
+pl.title('Grafica 1')
 pl.xlabel('Eje x')
 pl.ylabel('Eje y')
-pl.plot (x,y, marker='o',linestyle=':',color='r', label='funcion 1')
+pl.plot (x,y, marker='o',linestyle=':',color='r', label='Funcion (5^x)-5')
+pl.plot (x1,y1,color='k')
+pl.plot (x2,y2,color='k')
 pl.legend(loc=4)
-pl.savefig("grafica.eps", dpi=100)
+pl.xlim(-3.0, 3.0)
+pl.ylim(-6.0, 30.0)
+pl.xticks(x)
+pl.savefig("grafcap2.eps", dpi=100)
 pl.show()
